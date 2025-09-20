2a6862133dd42c6e62babf9591c4924d7434face
yotsatorn <yotsatorn.pa@gmail.com>
2025-09-20 18:25:43 +0700

    🏗️ build: configure path aliases ��️ for better module imports 📦 and project structure 🏢📐

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "@tailwindcss/vite";
-import { VitePWA } from "vite-plugin-pwa";
+import path from "node:path";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -163,4 +164,15 @@ export default defineConfig({
       },
     }),
   ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src/"),
+      "@core": path.resolve(__dirname, "./src/core/"),
+      "@features": path.resolve(__dirname, "./src/features/"),
+      "@shared": path.resolve(__dirname, "./src/shared"),
+      "@pages": path.resolve(__dirname, "./src/pages"),
+      "@config": path.resolve(__dirname, "./src/config"),
+      "@assets": path.resolve(__dirname, "./src/assets"),
+    },
+  },
 });
