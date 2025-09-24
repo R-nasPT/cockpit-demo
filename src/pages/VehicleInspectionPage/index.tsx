dfc073224e6452822ed81aad37dbcd9020684408
yotsatorn <yotsatorn.pa@gmail.com>
2025-09-24 20:30:05 +0700

    🚀✨ feat(form): add VehicleInspectionForm 🚙🛠️📝 with multi-step validation ✔️🛡️ and navigation 🧭🔀

--- src/pages/VehicleInspectionPage/index.tsx
+++ src/pages/VehicleInspectionPage/index.tsx
@@ -1,7 +1,5 @@
-import MultiStepForm from "@/features/MultiStep/option1/components/MultiStepForm";
-// import VehicleInspectionForm from "@/features/VehicleInspection/components/VehicleInspectionForm";
+import VehicleInspectionForm from "@/features/VehicleInspection/components/VehicleInspectionForm";
 
 export default function VehicleInspectionPage() {
-//   return <VehicleInspectionForm />;
-  return <MultiStepForm />;
+  return <VehicleInspectionForm />;
 }
