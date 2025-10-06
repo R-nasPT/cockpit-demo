850a8ab56f13f01df8679e4d30701d8897943478
yotsatorn <yotsatorn.pa@gmail.com>
2025-10-06 16:34:06 +0700

    delete line

--- src/core/router/routes.ts
+++ src/core/router/routes.ts
@@ -4,7 +4,6 @@ import { menuRoutes } from '@/features/mainMenu/routes';
 import { vehicleInspectionRoutes } from '@/features/VehicleInspection/routes';
 import RootLayout from '../layouts/RootLayout';
 // import { lazy } from "react";
-// import RootLayout from "@/shared/components/layout/RootLayout";
 
 // const LoginPage = lazy(() => import('@/features/auth/pages/LoginPage'));
 // const NotFoundPage = lazy(() => import("@/pages/NotFoundPage"));
